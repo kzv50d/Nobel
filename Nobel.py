00fcdfc3cc0e5093155e0f568032a2249986cd13
kzv50d <195387848+kzv50d@users.noreply.github.com>
2026-09-19 18:24:53 -0600

    Add files via upload

--- Nobel.py
+++ Nobel.py
@@ -22,7 +22,7 @@ def user_input_features():
 
 df = user_input_features()
 
-nobel =  pd.read_csv('nobel.csv', encoding='latin-1')
+nobel =  pd.read_csv('nobel2.csv', encoding='latin-1')
 X = nobel.Text
 y = nobel.Label
 
